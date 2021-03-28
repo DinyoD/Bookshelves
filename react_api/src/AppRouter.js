728d0b3a280eecb9025d0c9568af12ed648bb487
DinyoD <40523718+DinyoD@users.noreply.github.com>
2021-03-28 14:16:44 +0300

    Add Home component with some buttons for test.

--- react_api/src/AppRouter.js
+++ react_api/src/AppRouter.js
@@ -1,12 +1,12 @@
 import { Route} from 'react-router-dom';
 
-import Body from './components/Body';
+import Home from './components/Home';
 import Register from './components/Register';
 import Login from './components/Login';
 
 const AppRouter = () => (
     <div className='body-container'>
-        <Route path='/home' component={Body}/>
+        <Route path='/home' component={Home}/>
         <Route path='/register' component={Register}/>
         <Route path='/login' component={Login} />
     </div>
--- react_api/src/components/Home.js
+++ react_api/src/components/Home.js
@@ -1,11 +1,11 @@
 import Button from './Button';
 
-const  Body = ({pageName}) => (
+const  Home = ({pageName}) => (
         <div className='body'>
             <h2 className='body-title'>Hello from {pageName}</h2>
             <div>
-                <Button text='Add' color='white'/>
-                <Button text='Remove' color='white'/>
+                <Button text='Add' color='forestgreen'/>
+                <Button text='Remove' color='sandybrown'/>
             </div>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo iure voluptatibus labore non voluptates, voluptate qui corporis dolor tempore, inventore blanditiis maiores? Eaque mollitia omnis temporibus voluptatum dignissimos consequatur molestiae.</p>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo iure voluptatibus labore non voluptates, voluptate qui corporis dolor tempore, inventore blanditiis maiores? Eaque mollitia omnis temporibus voluptatum dignissimos consequatur molestiae.</p>
@@ -15,8 +15,8 @@ const  Body = ({pageName}) => (
 );
 
 
-Body.defaultProps ={
+Home.defaultProps ={
     pageName: 'Home'
 }
 
-export default Body;
+export default Home;
