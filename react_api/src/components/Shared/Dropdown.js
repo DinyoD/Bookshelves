4d31203ed60326d0f7fedc2a07eda469c5884f00
DinyoD <40523718+DinyoD@users.noreply.github.com>
2021-04-12 15:35:52 +0300

    Add change handler in Dropdown .

--- react_api/src/components/Shared/Dropdown.js
+++ react_api/src/components/Shared/Dropdown.js
@@ -1,16 +1,25 @@
 import {useState, useEffect} from 'react';
+import { useHistory } from 'react-router-dom';
 
-const 
-Dropdown = ({name, data}) => {
+const Dropdown = ({name, data}) => {
 
     const[collection, setCollection] = useState([]);
+    const history = useHistory();
 
     useEffect(()=>{
         setCollection([name, ...data])
-    },[data])   
+    },[data])
+
+    const ChangeValueHandler = (e) => {
+        let currValue = e.target.value;
+        if (currValue !== name) {
+            history.push(`/${name.toLowerCase()}/${currValue.toLowerCase()}`);
+            e.target.value = name;
+        }
+    }
 
     return (
-        <select className='header-nav-dropdown header-nav-link' defaultValue={name}>
+        <select className='header-nav-dropdown header-nav-link' onChange={ChangeValueHandler}>
 
             {collection.map((x, i)=> (<option className='dropdown-option' key={i} value={x}>{x}</option>))}
 
